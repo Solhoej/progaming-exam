2aa431d43a50c8bc2f6872aea711f4d1c1fb33e9
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2024-04-26 14:29:45 +0200

    Ui

--- sketch.js
+++ sketch.js
@@ -1,12 +1,38 @@
 let datastore = {};
 
+let SearchBar;
+let SearchButton;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth / 1.2, windowHeight / 1.2);
+  background(117, 94, 140, 20);
+  GUIApplications();
 }
 
 function draw() {
-  //background(16,53,95);
   GUI();
+}
+
+function GUIApplications()
+{
+  SearchBar = createInput();
+  SearchBar.attribute("placeholder", "Search for account number")
+  SearchBar.position(width/6, height/7)
+  SearchBar.size(200, 15)
+
+  
+
+}
+
+function Scrollframe(x=0, y=0)
+{
+  push();
+  translate(x, y)
+  noFill();
+  let backdrop = rect(0, 0, width/3, height)
+  fill(255);
+  let scroller = rect(width/3.10, 0, 10, height, 90);
+  pop();
 }
 
 function GUI()
@@ -14,6 +40,8 @@ function GUI()
   push();
   stroke(255)
   strokeWeight(5);
+
+  Scrollframe();
 
   //section1
   line(width/3, 0, width/3, height);
