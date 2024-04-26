87ce3bb9b934d189d93f6aa6806d45ed3893ba1b
Solhoej <142444308+Solhoej@users.noreply.github.com>
2024-04-26 14:29:50 +0200

    createaccount function

--- sketch.js
+++ sketch.js
@@ -12,6 +12,19 @@ function draw() {
   console.log(account1.account);
 }
 
+function createAccount()
+{
+  firstName = "Lars";
+  lastName = "Mikkelsen";
+  cprNumber = "2004006969";
+  address = "Squidgame Street 69";
+  balance = 10;
+
+  let newAccount = new Accounts(firstName, lastName, cprNumber, address, balance);
+  let key = newAccount.accountNumber;
+  datastore = {[key]:newAccount}
+}
+
 function GUI()
 {
   push();
