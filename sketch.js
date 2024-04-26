ed3e811d3aca7a9de71dd30e97c93fd33c37688c
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2024-04-26 15:11:37 +0200

    Make it look fancy

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 let datastore = {};
 
 let SearchBar;
-let SearchButton;
 
 function setup() {
   createCanvas(windowWidth / 1.2, windowHeight / 1.2);
@@ -18,8 +17,9 @@ function GUIApplications()
 {
   SearchBar = createInput();
   SearchBar.attribute("placeholder", "Search for account number")
-  SearchBar.position(width/6, height/7)
+  SearchBar.position(width/5, height/8)
   SearchBar.size(200, 15)
+  SearchBar.class("inputForm");
 
 }
 
@@ -28,9 +28,10 @@ function Scrollframe(x=0, y=0)
   push();
   translate(x, y)
   noFill();
-  let backdrop = rect(0, 0, width/3, height)
+  stroke(150)
+  let backdrop = rect(0, 0, (width/3) - x*2, height - 80)
   fill(255);
-  let scroller = rect(width/3.10, 0, 10, height, 90);
+  let scroller = rect(width/3.3, 0, 10, (height - 80), 90);
   pop();
 }
 
@@ -40,7 +41,7 @@ function GUI()
   stroke(255)
   strokeWeight(5);
 
-  Scrollframe();
+  Scrollframe(15, 65);
 
   //section1
   line(width/3, 0, width/3, height);
