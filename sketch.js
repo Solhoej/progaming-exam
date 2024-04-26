567f362ca993994a9f95683e36af067672e37ac4
ChristofferS22R <95274166+DamrongS@users.noreply.github.com>
2024-04-26 14:10:27 +0200

    Made it look cozy

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
+let datastore;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth / 1.2, windowHeight / 1.2);
 }
 
 function draw() {
-  background(220);
+  //background(16,53,95);
+  GUI();
+}
+
+function GUI()
+{
+  push();
+  stroke(255)
+  strokeWeight(5);
+
+  //section1
+  line(width/3, 0, width/3, height);
+  
+
+  //section2
+  line(width/1.5, 0, width/1.5, height);
+
+
+  //section3
+  pop();
+}
+
+function Datastore()
+{
+  datastore = {}
 }
